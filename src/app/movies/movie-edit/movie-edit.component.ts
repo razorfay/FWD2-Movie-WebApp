3173ec59465388d736740be282e080e048c49e65
Nixon Teh <0117999@kdu-online.com>
2018-11-18 17:14:36 +0800

    add movie card lists and edit component

--- src/app/movies/movie-edit/movie-edit.component.ts
+++ src/app/movies/movie-edit/movie-edit.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-movie-edit',
-  templateUrl: './movie-edit.component.html',
-  styleUrls: ['./movie-edit.component.scss']
+ selector: 'app-movie-edit',
+ templateUrl: './movie-edit.component.html',
+ styleUrls: ['./movie-edit.component.scss']
 })
 export class MovieEditComponent implements OnInit {
 
-  constructor() { }
+ movie$: Observable<Movie>;
+ movieDocument: AngularFirestoreDocument<Movie>;
+ movieCollection: AngularFirestoreCollection<Movie>;
 
-  ngOnInit() {
-  }
+ constructor(
+   private db: AngularFirestore,
+   private route: ActivatedRoute,
+   private router: Router
+ ) { }
+
+ ngOnInit() {
+   this.route.params.subscribe(params => {
+     this.movieDocument = this.db.doc<Movie>('movies/' + params.id);
+     this.movie$ = this.movieDocument.valueChanges();
+   });
+ }
+
+ updateMovie(movie) {
+   this.movieDocument.update(movie).then(() => {
+     this.router.navigate(['/movie']);
+   });
+ }
 
 }
+
+
+
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
 @Component({
-  selector: 'app-movies',
-  templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+ selector: 'app-movies',
+ templateUrl: './movies.component.html',
+ styleUrls: ['./movies.component.scss']
 })
 export class MoviesComponent implements OnInit {
 
-  constructor() { }
+ movies$: Observable<Movie[]>;
+ moviesCollection: AngularFirestoreCollection<Movie>;
 
-  ngOnInit() {
-  }
+ constructor(
+   private db: AngularFirestore
+ ) { }
 
+ ngOnInit() {
+   const orderBy = ref => ref.orderBy('rating', 'desc');
+   this.moviesCollection = this.db.collection<Movie>('movies', orderBy);
+   this.movies$ = this.moviesCollection
+     .snapshotChanges()
+     .pipe(
+       map(movies => {
+         return movies.map(movie => {
+           const data = movie.payload.doc.data() as Movie;
+           const id = movie.payload.doc.id;
+           return { ...data, id };
+         });
+       })
+     );
+ }
 }
+
+
