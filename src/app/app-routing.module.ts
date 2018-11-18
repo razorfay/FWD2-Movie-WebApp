d6cf259de82f2380833c533c86258f611b10a35d
Nixon Teh <0117999@kdu-online.com>
2018-11-18 16:00:20 +0800

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieCreateComponent } from './movies/movie-create/movie-create.component';
+import { MovieEditComponent } from './movies/movie-edit/movie-edit.component';
 
 const routes: Routes = [{
   path: 'movie',
-  component: MovieComponent
+  component: MoviesComponent
 }, {
   path: 'movie/create',
   component: MovieCreateComponent
 },{
   path: 'movie/edit/:id',
-  component: MoviesEditComponent
+  component: MovieEditComponent
 },{
   path: '**',
   redirectTo: 'movie'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieCreateComponent } from './movies/movie-create/movie-create.component';
+import { MovieEditComponent } from './movies/movie-edit/movie-edit.component';
+import { MovieFormComponent } from './movies/movie-form/movie-form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MovieCreateComponent,
+    MovieEditComponent,
+    MovieFormComponent
   ],
   imports: [
     BrowserModule,
